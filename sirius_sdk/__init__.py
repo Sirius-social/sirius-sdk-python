57ecd089aea9e1d7fccfc901cfb67e387275408c
Pavel Minenkov <minikspb@gmail.com>
2020-08-26 16:27:23 +0300

    refactoring

--- sirius_sdk/__init__.py
+++ sirius_sdk/__init__.py
@@ -1,6 +1,6 @@
-from .agent.agent import Agent
-from .encryption import P2PConnection
-from .agent.pairwise import Pairwise, TheirEndpoint
+from sirius_sdk.agent.agent import Agent
+from sirius_sdk.encryption import P2PConnection
+from sirius_sdk.agent.pairwise import Pairwise, TheirEndpoint
 
 
 __all__ = ["Agent", "P2PConnection", "Pairwise", "TheirEndpoint"]
