78caaef24d4919089f68ba5e90713e8bdb62cd57
Pavel Minenkov <minikspb@gmail.com>
2020-09-17 00:02:59 +0300

    refactoring

--- sirius_sdk/agent/connections.py
+++ sirius_sdk/agent/connections.py
@@ -135,7 +135,7 @@ class AgentRPC(BaseAgentConnection):
         self.__networks = []
         self.__websockets = {}
         self.__prefer_agent_side = True
-        self.__connector = aiohttp.TCPConnector(verify_ssl=False)
+        self.__connector = aiohttp.TCPConnector(verify_ssl=False, keepalive_timeout=60)
 
     @property
     def endpoints(self) -> List[Endpoint]:
