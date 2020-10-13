50d16fc6b136cf78302a78a02f6af6eaa6021910
Pavel Minenkov <minikspb@gmail.com>
2020-10-13 17:30:24 +0300

    refactoring

--- sirius_sdk/agent/aries_rfc/feature_0113_question_answer/recipes.py
+++ sirius_sdk/agent/aries_rfc/feature_0113_question_answer/recipes.py
@@ -1,27 +1,43 @@
-import uuid
+import datetime
+from typing import Optional
 
 import sirius_sdk
-from sirius_sdk.errors.exceptions import SiriusTimeoutIO
 from sirius_sdk.hub import CoProtocolThreaded
 from sirius_sdk.agent.pairwise import Pairwise
 
 from .messages import Question, Answer
 
 
-async def ask_and_wait_answer(query: Question, to: Pairwise) -> Answer:
+async def ask_and_wait_answer(query: Question, to: Pairwise) -> (bool, Optional[Answer]):
+    iso_string = query.expires_time
+    if iso_string:
+        try:
+            expires_at = datetime.datetime.fromisoformat(iso_string)
+            expires_at = expires_at.replace(tzinfo=datetime.timezone.utc)
+            delta = expires_at.replace(tzinfo=None) - datetime.datetime.utcnow().replace(tzinfo=None)
+            ttl = round(delta.total_seconds())
+        except Exception as e:
+            ttl = None
+    else:
+        ttl = None
+
     co = CoProtocolThreaded(
         thid=query.id,
         to=to,
-        protocols=[Question.PROTOCOL]
+        protocols=[Question.PROTOCOL],
+        time_to_live=ttl
     )
-    success, answer = await co.switch(query)
+    try:
+        success, answer = await co.switch(query)
+    except Exception as e:
+        raise
     if success:
         if isinstance(answer, Answer):
-            return answer
+            return True, answer
         else:
             raise RuntimeError('Unexpected msg type')
     else:
-        raise SiriusTimeoutIO('Operation terminated by timeout')
+        return False, None
 
 
 async def make_answer(response: str, q: Question, to: Pairwise):
