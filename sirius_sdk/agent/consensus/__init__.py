e1b1adcd621d3e55c1f61210200498384a4eb02d
Pavel Minenkov <minikspb@gmail.com>
2021-03-12 18:16:01 +0300

    implement distributed locking of the MicroLedgers

--- sirius_sdk/agent/consensus/__init__.py
+++ sirius_sdk/agent/consensus/__init__.py
@@ -7,7 +7,8 @@ class Locking:
 
     NAMESPACE = 'ledgers'
 
-    async def acquire(self, names: List[str], lock_timeout: float) -> (bool, List[str]):
+    @classmethod
+    async def acquire(cls, names: List[str], lock_timeout: float) -> (bool, List[str]):
         """Lock ledgers given by names.
 
         :names: names of microledgers
@@ -15,13 +16,14 @@ class Locking:
         """
         ledger_names = names[:]  # copy
         ledger_names = list(set(ledger_names))  # remove duplicates
-        ledger_resources = [f'{self.NAMESPACE}/{name}' for name in ledger_names]
+        ledger_resources = [f'{cls.NAMESPACE}/{name}' for name in ledger_names]
         ok, locked_ledgers = await acquire(resources=ledger_resources, lock_timeout=lock_timeout)
         locked_ledgers = [item.split('/')[-1] for item in locked_ledgers]  # remove namespace prefix
         return ok, locked_ledgers
         pass
 
-    async def release(self):
+    @classmethod
+    async def release(cls):
         """Released all resources locked in current context"""
         await release()
         pass
