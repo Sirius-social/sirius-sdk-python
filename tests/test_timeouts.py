635bdad5d4055227fcaf31f3641ff725958e6b64
Pavel <minikspb@gmail.com>
2021-09-20 14:06:50 +0300

    *

--- tests/test_timeouts.py
+++ tests/test_timeouts.py
@@ -2,5 +2,10 @@ import pytest
 
 
 @pytest.mark.asyncio
+async def test_agent_rcv_timeout():
+    assert 0, 'TODO'
+
+
+@pytest.mark.asyncio
 async def test_coprotocol_timeout():
     assert 0, 'TODO'
