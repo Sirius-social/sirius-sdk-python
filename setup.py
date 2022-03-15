9bf1ff03dcdc6aabd8189765dcbed0733e2e0c5b
Pavel <minikspb@gmail.com>
2022-03-15 20:05:46 +0300

    rebuild mechanism for SDK init

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(HERE, "README.rst")) as fid:
 
 setup(
     name="sirius-sdk",
-    version="1.4.1",  # + os.getenv('BUILD_NUMBER'),
+    version="1.5.1",  # + os.getenv('BUILD_NUMBER'),
     description="SDK for developing Smart-Contracts in Self-sovereign-identity world.",
     long_description=README,
     long_description_content_type="text/x-rst",
